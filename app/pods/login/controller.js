89f4e9f7fad66e805c67317da3bd01a0a094d5aa
Jibran Kalia <jibran.kalia@gmail.com>
2019-11-28 23:13:32 -0600

    Better login screen

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -1,17 +1,26 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
+const ERROR_MAPPER = Object.freeze({
+  '401': 'Invalid login credentials. Please try again.'
+});
+
 export default Controller.extend({
   session: service(),
+
+
+  handleError(error) {
+    const msg = ERROR_MAPPER[error.status.toString()]
+    this.set('errorMessage', [msg]);
+  },
 
   actions: {
     async authenticate() {
       let { identification, password } = this.getProperties('identification', 'password');
       try {
         await this.session.authenticate('authenticator:devise-token-auth', identification, password);
-      } catch(e) {
-        console.error(e)
-        this.set('errorMessage', e);
+      } catch(error) {
+        this.handleError(error);
       }
 
       if (this.session.isAuthenticated) {

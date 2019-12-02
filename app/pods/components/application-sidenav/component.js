337f2e72d1fdcd43892d2c72329c74d710d59caf
Jibran Kalia <jibran.kalia@gmail.com>
2019-12-02 07:23:11 -0600

    Toolbar is working as expected

--- app/pods/components/application-sidenav/component.js
+++ app/pods/components/application-sidenav/component.js
@@ -3,12 +3,15 @@ import { inject as service } from '@ember/service';
 
 export default Component.extend({
   currentUser: service(),
-  session: service(),
   router: service(),
+  session: service(),
 
   actions: {
     transitionToClassrooms() {
       this.router.transitionTo('home.classrooms');
+    },
+    invalidateSession() {
+      this.session.invalidate();
     }
   }
 })

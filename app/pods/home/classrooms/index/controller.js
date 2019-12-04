8e316575a15f56eb01e24a709401e888e0c80f23
Jibran Kalia <jibran.kalia@gmail.com>
2019-12-03 21:36:44 -0600

    Initial create class modal

--- app/pods/home/classrooms/index/controller.js
+++ app/pods/home/classrooms/index/controller.js
@@ -3,6 +3,39 @@ import { readOnly } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  store: service(),
   currentUser: service(),
+  paperToaster: service(),
   user: readOnly('currentUser.user'),
+
+  showCreateClassModal: false,
+
+  actions: {
+    openCreateClassModal() {
+      this.set('showCreateClassModal', true);
+    },
+    closeCreateClassModal() {
+      this.set('showCreateClassModal', false);
+    },
+    async saveClassroom(className) {
+      try {
+        const classroom = this.store.createRecord('classroom', {
+          name: className,
+          user: this.user
+        })
+        await classroom.save();
+        this.paperToaster.show('Class Created', {
+          duration: 4000,
+          toastClass: this.get('toastClass')
+        });
+
+      } catch(e) {
+        console.error(e);
+
+      } finally {
+        this.set('showCreateClassModal', false);
+      }
+    }
+
+  }
 });

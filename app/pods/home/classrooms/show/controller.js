60771a606076572290e60e1f46ca11ad5b08e8b2
Jibran Kalia <jibran.kalia@gmail.com>
2019-12-07 00:34:22 -0600

    Slightly more work

--- app/pods/home/classrooms/show/controller.js
+++ app/pods/home/classrooms/show/controller.js
@@ -1,3 +1,21 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { readOnly } from '@ember/object/computed';
 
-export default Controller.extend({});
+export default Controller.extend({
+  store: service(),
+  currentUser: service(),
+  paperToaster: service(),
+  user: readOnly('currentUser.user'),
+
+  actions: {
+    showEnrollmentDetails(enrollmentId, event) {
+      if (!event.target.classList.contains("md-button")) {
+        this.transitionToRoute('home.enrollments.show', enrollmentId);
+      }
+    },
+    deleteEnrollment(enrollment) {
+      enrollment.destroyRecord();
+    }
+  }
+})

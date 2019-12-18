03945d1983a4a14983233ca526dad8c007437204
Jibran Kalia <jibran.kalia@gmail.com>
2019-12-17 23:04:06 -0600

    Surah selectors looking better

--- app/pods/components/lesson-selector/surah/component.js
+++ app/pods/components/lesson-selector/surah/component.js
@@ -1,8 +1,14 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Component.extend({
   constants: service(),
+
+  selectedSurah: computed('lesson.{fromSurah,toSurah}', function() {
+    const surahNumber = this.from ? this.lesson.fromSurah : this.lesson.toSurah;
+    return this.constants.SURAHS.find(r => r.number === surahNumber)
+  }),
 
   actions: {
     setSurah(surah) {

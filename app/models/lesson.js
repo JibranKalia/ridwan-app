e0bb193caf7500530059d8af3e6ac8601ed524ab
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-04 23:02:41 -0600

    Adding moment

--- app/models/lesson.js
+++ app/models/lesson.js
@@ -1,13 +1,14 @@
 import Model, { attr } from '@ember-data/model';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default Model.extend({
   // attributes
   date: attr('date'),
   // computed properties
   formattedDate: computed('date', function() {
-    return this.date.toLocaleDateString("en-US");
+    return moment(this.date).format('L')
   }),
 
   // relationships

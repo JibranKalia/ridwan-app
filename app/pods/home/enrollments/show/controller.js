76c3712a7c8ae62e52be83caaa8eba78343509e8
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-05 21:13:34 -0600

    Showing older lessons correctly

--- app/pods/home/enrollments/show/controller.js
+++ app/pods/home/enrollments/show/controller.js
@@ -1,4 +1,9 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default Controller.extend({
+  nonCurrentLessons: computed('model.enrollment.lessons.[]', function() {
+    return this.model.enrollment.lessons.filter((lesson) => moment().isAfter(lesson.date, 'day'));
+  }),
 });

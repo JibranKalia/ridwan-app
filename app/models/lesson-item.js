b612d3db4f1d699eabffca41ad62680626cab872
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-06 19:39:26 -0600

    Fixing lesson tile text

--- app/models/lesson-item.js
+++ app/models/lesson-item.js
@@ -1,13 +1,31 @@
 import Model, { attr } from '@ember-data/model';
 import { belongsTo } from 'ember-data/relationships';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Model.extend({
+  constants: service(),
+
   // attributes
   name: attr('string'),
   fromSurah: attr('number'),
   fromAyah: attr('number'),
   toSurah: attr('number'),
   toAyah: attr('number'),
+
+  // computed properties
+  fromSurahObject: computed('fromSurah', function() {
+    return this.constants.SURAHS.find(r => r.number === this.fromSurah);
+  }),
+  toSurahObject: computed('toSurah', function() {
+    return this.constants.SURAHS.find(r => r.number === this.toSurah);
+  }),
+  fromAyahString: computed('fromAyah', function() {
+    return this.fromAyah.toString().padStart(2, '0');
+  }),
+  toAyahString: computed('toAyah', function() {
+    return this.toAyah.toString().padStart(2, '0');
+  }),
 
   // relationships
   lesson: belongsTo('lesson')
--- app/pods/components/lessons/tile/text/component.js
+++ app/pods/components/lessons/tile/text/component.js
@@ -1,5 +1,5 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['layout-column']
+  classNames: ['layout-column', 'lessons-tile-text']
 })

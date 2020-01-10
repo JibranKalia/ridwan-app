896030ad360cdd29ff4da594df5a447c7c690f65
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-09 23:07:19 -0600

    Adding ember data store front

--- app/models/classroom.js
+++ app/models/classroom.js
@@ -1,8 +1,9 @@
 import Model, { attr } from '@ember-data/model';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 import { mapBy } from '@ember/object/computed';
+import LoadableModel from 'ember-data-storefront/mixins/loadable-model';
 
-export default Model.extend({
+export default Model.extend(LoadableModel, {
   // attributes
   name: attr('string'),
   // computed properties
--- app/pods/home/classrooms/index/route.js
+++ app/pods/home/classrooms/index/route.js
@@ -4,7 +4,7 @@ import { hash } from 'rsvp';
 export default Route.extend({
   model() {
     return hash({
-      classrooms: this.store.query('classroom', {})
+      classrooms: this.store.loadRecords('classroom')
     })
   },
 });

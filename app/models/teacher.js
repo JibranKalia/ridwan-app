e849d9406b5005ab50b846db3bc0554cb758f7d4
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-15 12:57:34 -0600

    Moving name to users

--- app/models/teacher.js
+++ app/models/teacher.js
@@ -1,17 +1,6 @@
-import { mapBy } from '@ember/object/computed';
-import Model, { attr, belongsTo } from '@ember-data/model';
-import { computed } from '@ember/object';
+import Model, { belongsTo } from '@ember-data/model';
 
 export default Model.extend({
-  // attributes
-  firstName: attr('string'),
-  lastName: attr('string'),
-  // computed properties
-  firstNameErrors: mapBy('errors.firstName', 'message'),
-  lastNameErrors: mapBy('errors.lastName', 'message'),
-  formattedName: computed('firstName,lastName', function() {
-    return `${this.firstName} ${this.lastName}`;
-  }),
   // relationships
   user: belongsTo('user')
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,19 @@
 import Model, { attr, belongsTo } from '@ember-data/model';
+import { mapBy } from '@ember/object/computed';
+import { computed } from '@ember/object';
 
 export default Model.extend({
   // attributes
   email: attr('string'),
   type: attr('string'),
-   
+  firstName: attr('string'),
+  lastName: attr('string'),
+  // computed properties
+  firstNameErrors: mapBy('errors.firstName', 'message'),
+  lastNameErrors: mapBy('errors.lastName', 'message'),
+  formattedName: computed('firstName,lastName', function() {
+    return `${this.firstName} ${this.lastName}`;
+  }),
   // relationships
   teacher: belongsTo('teacher')
 });

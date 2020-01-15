f7947b214483cbb06accb57e125dff1ac6a7221b
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-15 10:39:21 -0600

    Adding current user to rollbar

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -5,13 +5,16 @@ import { resolve } from 'rsvp';
 export default Service.extend({
   session: service(),
   store: service(),
+  rollbar: service(),
 
   load() {
     let userId = this.get('session.data.authenticated.user_id');
     if (!isEmpty(userId)) {
-      return this.store.findRecord('user', userId).then((user) => {
-        this.set('user', user);
-      });
+      return this.store.findRecord('user', userId)
+        .then((user) => {
+          this.set('user', user);
+          this.set('rollbar.currentUser', { email: user.email, id: user.id });
+        });
     } else {
       return resolve();
     }

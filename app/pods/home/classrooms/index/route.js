fff3cd4693e651e7c9afdd24c49e99290812e980
Jibran Kalia <jibran.kalia@gmail.com>
2020-01-19 16:17:45 -0600

    classrooms require authenticated

--- app/pods/home/classrooms/index/route.js
+++ app/pods/home/classrooms/index/route.js
@@ -1,7 +1,8 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
   model() {
     return hash({
       classrooms: this.store.loadRecords('classroom')
